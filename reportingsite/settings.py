d9c75dde5a0ecfc1f644aa7db299a492fff52d5d
Arjuna Bazaz <arjunabazaz@gmail.com>
2024-04-30 21:12:05 -0400

    fixed settings issue

--- reportingsite/settings.py
+++ reportingsite/settings.py
@@ -164,8 +164,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.environ.get("EMAIL_HOST")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "cs3240a23reportingsite@gmail.com"
-EMAIL_HOST_PASSWORD = "flkpptmacjiroowa"
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("flkpptmacjiroowa")
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
